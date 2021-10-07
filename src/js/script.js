0b63141fd44a6d17f19e136c688b48baf4d355fd
Johnny Gregório <johkker@gmail.com>
2021-10-07 13:28:38 -0300

    Corrigido funçoes de vitoria

--- src/js/script.js
+++ src/js/script.js
@@ -13,8 +13,7 @@ function startRestart() {
     let body = document.getElementsByTagName("body")
     body[0].setAttribute("class", "playerOne--bg")
 
-    let board = document.getElementsByClassName("")
-
+    
 
     boardSize = [
         ["X", "X", "X", "X", "X", "X"],
@@ -31,9 +30,13 @@ function startRestart() {
         start.innerText = "RESET"
     } else {
         reset()
+
     }
 }
 
+let firstPlayer = true
+
+let drawCounter = 0
 
 function reset() {
     // A primeira coluna é a esquerda
@@ -43,8 +46,11 @@ function reset() {
         cells[k].innerHTML = ""
         cells[k].classList.add("playerOne--board")
     }
+
     firstPlayer = true
+    drawCounter = 0
 }
+
 
 // Cria o tabuleiro com as classes e ids prontas
 function createBoard() {
@@ -63,37 +69,37 @@ function createBoard() {
     }
 }
 
-// Armazena o estado de quem é a vez de jogar
-let firstPlayer = true
 
 // Base da função do jogo todo
 function play(e) {
     //Verifica se o movimento é válido ou não
     if (e.currentTarget.lastChild.childElementCount === 0) {
+        drawCounter++
         // Verifica se é o Player 1 ou 2
         if (firstPlayer === true) {
             insertDisk(e.currentTarget, firstPlayer)
-            
+
             let body = document.getElementsByTagName("body")
             body[0].setAttribute("class", "playerTwo--bg")
 
             let cells = document.getElementsByTagName("li")
-            for(let i=0; i< cells.length; i++){
+            for (let i = 0; i < cells.length; i++) {
                 cells[i].setAttribute("class", "game__board__cell playerTwo--board")
             }
 
             firstPlayer = false
-            
+
+
         } else {
             insertDisk(e.currentTarget, firstPlayer)
 
             let body = document.getElementsByTagName("body")
             body[0].setAttribute("class", "playerOne--bg")
-        
+
             let cells = document.getElementsByTagName("li")
-            for(let i=0; i< cells.length; i++){
+            for (let i = 0; i < cells.length; i++) {
                 cells[i].setAttribute("class", "game__board__cell playerOne--board")
-                
+
             }
 
             firstPlayer = true
@@ -104,123 +110,149 @@ function play(e) {
 
 // Matheus
 
-function checkHorizontalVictory(arrayLine,arrayColumn){
-    let counter={
-        Horizontal:{left:0,right:0,leftAble:true,rightAble:true},
+function checkHorizontalVictory(arrayLine, arrayColumn) {
+    let counter = {
+        Horizontal: {
+            left: 0,
+            right: 0,
+            leftAble: true,
+            rightAble: true
+        },
     };
-    
+
     let level = 1
-    while (level<4){
-        if(arrayLine-level>=0){
-            if(boardSize[arrayLine-level][arrayColumn]===boardSize[arrayLine][arrayColumn]&&counter.Horizontal.leftAble===true){
+    while (level < 4) {
+        if (arrayLine - level >= 0) {
+            if (boardSize[arrayLine - level][arrayColumn] === boardSize[arrayLine][arrayColumn] && counter.Horizontal.leftAble === true) {
                 counter.Horizontal.left++
-            }else{
-                counter.Horizontal.leftAble===false
+                /*console.log(boardSize[arrayLine - level][arrayColumn])
+                console.log(boardSize[arrayLine][arrayColumn])*/
+            } else {
+                counter.Horizontal.leftAble = false
             }
         }
-        
-        if(arrayLine+level<=6){
-            if(boardSize[arrayLine+level][arrayColumn]===boardSize[arrayLine][arrayColumn]&&counter.Horizontal.rightAble===true){
+
+        if (arrayLine + level <= 6) {
+            if (boardSize[arrayLine + level][arrayColumn] === boardSize[arrayLine][arrayColumn] && counter.Horizontal.rightAble === true) {
                 counter.Horizontal.right++
-            }else{
-                counter.Horizontal.rightAble===false
+                /*console.log(boardSize[arrayLine + level][arrayColumn])
+                console.log(boardSize[arrayLine][arrayColumn])*/
+            } else {
+                counter.Horizontal.rightAble = false
+                console.log()
             }
         }
-        
+
         level++
     }
 
-    let countHorizontal=counter.Horizontal.left+counter.Horizontal.right
-    
-    if(countHorizontal >= 3){
+    let countHorizontal = counter.Horizontal.left + counter.Horizontal.right
+
+    if (countHorizontal >= 3) {
         console.log(`Venceu a partida na horizontal`)
-        return true
-    }else {
-        return false
+        victoryStatus = true
+
     }
 }
 
-function checkVerticalVictory(arrayLine,arrayColumn){
-    let counter={
-        Vertical:{up:0},
+function checkVerticalVictory(arrayLine, arrayColumn) {
+    let counter = {
+        Vertical: {
+            up: 0
+        },
     };
-    
+
     let level = 1
-    while (level<4){
-        
-        if(boardSize[arrayLine][arrayColumn-level]===boardSize[arrayLine][arrayColumn]){
+    while (level < 4) {
+
+        if (boardSize[arrayLine][arrayColumn - level] === boardSize[arrayLine][arrayColumn]) {
             counter.Vertical.up++
         }
-        
+
         level++
     }
 
-    let countVertical=counter.Vertical.up
-    
-    if(countVertical >= 3){
+    let countVertical = counter.Vertical.up
+
+    if (countVertical >= 3) {
         console.log(`Venceu a partida na vertical`)
-        return true
-    }else {
-        return false
+        victoryStatus = true
+
     }
 }
 
-function checkDiagonalVictory(arrayLine,arrayColumn){
-    let counter={
-        DiagonalUp:{left:0,right:0,leftAble:true,rightAble:true},
-        DiagonalDown:{left:0,right:0,leftAble:true,rightAble:true}
+function checkDiagonalVictory(arrayLine, arrayColumn) {
+    let counter = {
+        DiagonalUp: {
+            left: 0,
+            right: 0,
+            leftAble: true,
+            rightAble: true
+        },
+        DiagonalDown: {
+            left: 0,
+            right: 0,
+            leftAble: true,
+            rightAble: true
+        }
     };
-    
+
     let level = 1
-    while (level<4){
-        if(arrayLine-level>=0){
-            if(boardSize[arrayLine-level][arrayColumn-level]===boardSize[arrayLine][arrayColumn]&&counter.DiagonalUp.leftAble===true){
+    while (level < 4) {
+        if (arrayLine - level >= 0) {
+            if (boardSize[arrayLine - level][arrayColumn - level] === boardSize[arrayLine][arrayColumn] && counter.DiagonalUp.leftAble === true) {
                 counter.DiagonalUp.left++
-            }else{
-                counter.DiagonalUp.leftAble===false
-            } 
+            } else {
+                counter.DiagonalUp.leftAble = false
+            }
         }
-        if(arrayLine+level<=6){
-            if(boardSize[arrayLine+level][arrayColumn+level]===boardSize[arrayLine][arrayColumn]&&counter.DiagonalUp.rightAble===true){
+        if (arrayLine + level <= 6) {
+            if (boardSize[arrayLine + level][arrayColumn + level] === boardSize[arrayLine][arrayColumn] && counter.DiagonalUp.rightAble === true) {
                 counter.DiagonalUp.right++
-            }else{
-                counter.DiagonalUp.rightAble===false
-            } 
-        }
-        
-        if (arrayLine-level>=0 && arrayColumn+level<=6){
-            if(boardSize[arrayLine-level][arrayColumn+level]===boardSize[arrayLine][arrayColumn]&&counter.DiagonalDown.leftAble===true){
-                counter.DiagonalDown.left++
-            }else{
-                counter.DiagonalDown.leftAble===false
+            } else {
+                counter.DiagonalUp.rightAble = false
             }
         }
 
-        if(arrayLine+level<=6 && arrayColumn-level>=0){
-            if(boardSize[arrayLine+level][arrayColumn-level]===boardSize[arrayLine][arrayColumn]&&counter.DiagonalDown.rightAble===true){
-                counter.DiagonalDown.right++
-            }else{
-                counter.DiagonalDown.rightAble===false
-            }  
+        if (arrayLine - level >= 0 && arrayColumn + level <= 6) {
+            if (boardSize[arrayLine - level][arrayColumn + level] === boardSize[arrayLine][arrayColumn] && counter.DiagonalDown.leftAble === true) {
+                counter.DiagonalDown.left++
+            } else {
+                counter.DiagonalDown.leftAble = false
+            }
         }
-    
+
+        if (arrayLine + level <= 6 && arrayColumn - level >= 0) {
+            if (boardSize[arrayLine + level][arrayColumn - level] === boardSize[arrayLine][arrayColumn] && counter.DiagonalDown.rightAble === true) {
+                counter.DiagonalDown.right++
+            } else {
+                counter.DiagonalDown.rightAble = false
+            }
+        }
+
         level++
     }
 
-    let countDiagonalUp=counter.DiagonalUp.left+counter.DiagonalUp.right
-    let countDiagonalDown=counter.DiagonalDown.left+counter.DiagonalDown.right
-    
-    if(countDiagonalUp >= 3 || countDiagonalDown >= 3){
+    let countDiagonalUp = counter.DiagonalUp.left + counter.DiagonalUp.right
+    let countDiagonalDown = counter.DiagonalDown.left + counter.DiagonalDown.right
+
+    if (countDiagonalUp >= 3 || countDiagonalDown >= 3) {
         console.log(`Venceu a partida na diagonal`)
-        return true
-    }else {
-        return false
+        victoryStatus = true
+
     }
+}
+
+let victoryStatus = false
+
+function draw() {
+    console.log("Empate!")
+    
 }
 // Matheus
 
 
-function insertDisk (selectedColumn, player) {
+function insertDisk(selectedColumn, player) {
     const columnIndex = selectedColumn.lastChild.getAttribute("id").slice(0, 1);
     const boardSizeLineIndex = Number(columnIndex) - 1;
     const boardSizeLine = boardSize[boardSizeLineIndex];
@@ -234,26 +266,30 @@ function insertDisk (selectedColumn, player) {
         diskValue = "P"
         disk.classList.add("black__piece");
     }
-    
+
     let attemptFindEmptyCell = 0;
-    
+
     for (let i = 0; i < boardSizeLine.length; i++) {
         attemptFindEmptyCell++
-        
+
         if (boardSizeLine[i] === 'X') {
-            
+
             boardSizeLine[i] = diskValue;
             const id = `${columnIndex}-${i+1}`;
             const emptyCell = document.getElementById(id);
-            
-            showFallAnimation(selectedColumn,disk,emptyCell,attemptFindEmptyCell)
-            
+
+            showFallAnimation(selectedColumn, disk, emptyCell, attemptFindEmptyCell)
+
             emptyCell.appendChild(disk);
-            
-            checkHorizontalVictory(boardSizeLineIndex,i);
-            checkVerticalVictory(boardSizeLineIndex,i);
-            checkDiagonalVictory(boardSizeLineIndex,i);
-            
+
+            checkHorizontalVictory(boardSizeLineIndex, i);
+            checkVerticalVictory(boardSizeLineIndex, i);
+            checkDiagonalVictory(boardSizeLineIndex, i);
+
+            if (drawCounter === 42 && victoryStatus === false) {
+                draw()
+            }
+
             break
         }
 
@@ -261,20 +297,19 @@ function insertDisk (selectedColumn, player) {
 
 }
 
-function showFallAnimation (column,disk,cell,counter){
-    
+function showFallAnimation(column, disk, cell, counter) {
+
     const columnHeight = column.clientHeight;
     const cellHeight = cell.clientHeight
-    const gap = columnHeight/6 - cellHeight
-    
+    const gap = columnHeight / 6 - cellHeight
+
     disk.classList.add("position-abs");
     disk.style.top = `-${cellHeight}px`
-    
+
     setTimeout(() => disk.style.top = `${cellHeight*(7-counter) - (gap*(counter-1))}px`, 300)
     setTimeout(() => {
         disk.removeAttribute("style");
         disk.classList.remove("position-abs");
     }, 1300)
-            
-}
 
+}
